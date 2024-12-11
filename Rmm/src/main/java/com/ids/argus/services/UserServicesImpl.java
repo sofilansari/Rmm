cc01a398b813a22522d4786c3c3ae931649e33e5
sofilansari <mdsofilansari@gmail.com>
2024-12-11 14:46:54 +0530

    services class

--- Rmm/src/main/java/com/ids/argus/services/UserServicesImpl.java
+++ Rmm/src/main/java/com/ids/argus/services/UserServicesImpl.java
@@ -1,10 +1,13 @@
 package com.ids.argus.services;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ids.argus.dto.LoginRequest;
 import com.ids.argus.dto.RegisterRequest;
 import com.ids.argus.dto.UserDto;
+import com.ids.argus.model.User;
+import com.ids.argus.repo.UserRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -12,18 +15,44 @@ import jakarta.transaction.Transactional;
 @Transactional
 public class UserServicesImpl implements UserServices{
 	
+	private final UserRepository userRepository;
 	
+	private final PasswordEncoder passwordEncoder;
+	
+	
+	
+
+	public UserServicesImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	@Override
 	public UserDto registerUser(RegisterRequest registerRequest) {
-		// TODO Auto-generated method stub
-		return null;
+		String encodedPassword =passwordEncoder.encode(registerRequest.getPassword());
+		
+		User users=new User();
+		
+		users.setUseName(registerRequest.getUseName());
+		users.setEmailId(registerRequest.getEmailId());
+		users.setPassword(encodedPassword);
+		users.setResetPaaword(registerRequest.getResetPaaword());
+		
+		User saveUser=userRepository.save(users);
+		
+		return new UserDto(saveUser.getPassword(), saveUser.getEmailId());
 	}
 
 	@Override
 	public UserDto loginUser(LoginRequest loginRequest) {
-		// TODO Auto-generated method stub
-		return null;
+		User user=userRepository.findByEmailId(loginRequest.getEmailId())
+				.orElseThrow(() ->new RuntimeException("User not found"));
+		if(!passwordEncoder.matches(loginRequest.getEmailId(), user.getEmailId())) {
+			
+			throw new RuntimeException("Invalid credentials");
+		}
+		
+		return new UserDto(user.getEmailId(), user.getUseName());
 	}
 
 }
